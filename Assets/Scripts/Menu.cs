3bb0d1f2395af45df5adb4e8708d33c5551251b9
Maxim Storozhenko <maxim.storojenko@gmail.com>
2021-10-14 17:52:47 +0300

    Add rules

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,14 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    private GameObject ReturnButton;
+    private GameObject View;
+
     public void StartButtonClick()
     {
         SceneManager.LoadScene("SampleScene");
     }
 
+    public void RulesButtonClick()
+    {
+        View.SetActive(true);
+        ReturnButton.SetActive(true);
+    }
+
+    public void ExitButtonClick()
+    {
+        Application.Quit();
+    }
+
+    public void ReturnButtonClick()
+    {
+        View.SetActive(false);
+        ReturnButton.SetActive(false);
+    }
+
     void Start()
     {
-        
+        View = GameObject.Find("Scroll View");
+        ReturnButton = GameObject.Find("ReturnButton");
+
+        View.SetActive(false);
+        ReturnButton.SetActive(false);
     }
 
     // Update is called once per frame
